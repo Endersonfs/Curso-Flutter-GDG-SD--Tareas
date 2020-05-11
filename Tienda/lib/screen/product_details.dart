32d4eb47bb40a3f32a464f765190de998ebffec5
Endersonfs <ender@dix.do>
2020-05-11 15:36:13 -0400

    Agregegando producdetails

--- Tienda/lib/screen/product_details.dart
+++ Tienda/lib/screen/product_details.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
 class ProducDetails extends StatelessWidget {
+  static const String routname ="producdetails";
+  ProductDetails();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        title: Row(
+          children:<Width>[
+            Icon(Icons.shopping_basket),
+            SizedBox(Width: 20),
+            Text("Detalles de producto")
+          ]
+        ); 
+      ),
+      body: Center(child: Text(""),)
     );
   }
 }
