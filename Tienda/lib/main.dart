610a36b5ba504cf24d0c8e83ecf0442088ed96f2
Endprog <ender@endprog.com>
2020-05-12 00:40:29 +0200

    Agregando clase shopping car

--- Tienda/lib/main.dart
+++ Tienda/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:Tienda/screen/home_screen.dart';
 import 'package:Tienda/screen/product_details.dart';
 import 'package:Tienda/screen/products_screen.dart';
+import 'package:Tienda/screen/shopping_car.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -19,6 +20,7 @@ class MyApp extends StatelessWidget {
           // HomeScreen.routename: (_)=> HomeScreen(),
           ProductScreen.routename: (_)=>ProductScreen(),
           ProducDetails.routename: (_)=>ProducDetails(),
+          ShoppingCar.routename: (_)=>ShoppingCar()
         },
     );
   }
---
+++ Tienda/lib/screen/shopping_car.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ShoppingCar extends StatelessWidget {
+  static const String routename ="shoppingcar";
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
