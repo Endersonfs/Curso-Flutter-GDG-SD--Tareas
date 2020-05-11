187695cee716c46f348ff984ab4f4654ab841248
Endprog <ender@endprog.com>
2020-05-12 00:55:35 +0200

    Crear Pantalla del Shopping Cart - agregando descripcion

--- Tienda/lib/screen/shopping_car.dart
+++ Tienda/lib/screen/shopping_car.dart
@@ -4,8 +4,19 @@ class ShoppingCar extends StatelessWidget {
   static const String routename ="shoppingcar";
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          children: <Widget>[
+            Icon(Icons.shopping_cart),
+            SizedBox(width: 20,),
+            Text("Lista de Productos")
+          ],
+        ),
+      ),
+      body: Center(
+        Text("Lista de productos a comprar")
+      ),
     );
   }
 }
